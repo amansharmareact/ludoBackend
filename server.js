49ff83ae1a03046de7e88a3ce4b43770b555a506
lijinlawrence <kittulawrence@gmail.com>
2025-08-22 16:57:48 +0530

    cors updated

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const roomRoutes = require("./route/roomRoutes");
 const Room = require("./models/room");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", adminRoutes);
